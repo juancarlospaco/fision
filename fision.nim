8f6191040202f5736b2099c6fa92b5c283bb49fe
Juan Carlos <JuanCarlospaco@gmail.com>
2020-06-22 04:10:14 -0300

    init

--- fision.nim
+++ fision.nim
@@ -1,16 +1,19 @@
 import strutils, sequtils, algorithm, strformat
 
-static:
-  const
-    blockList = ["comprehension", "with"] # Packages with problems to install of some kind.
-    url = "https://raw.githubusercontent.com/nim-lang/Nim/devel/testament/important_packages.nim"
-    code = staticExec("curl " & url).strip.splitLines
-    filter0 = filterIt(code, (it.startsWith("pkg1 ") or it.startsWith("pkg2 ")) and (it.contains(", false") or not it.contains(", true")))
-    filter1 = map(filter0, proc(s: string): string = s.toLowerAscii.split(", false" )[0].replace("pkg1 ", "").replace("pkg2 ", "").strip)
-    packages = filterIt(filter1, it notin blockList).sorted.join(",\n  ")
-  writeFile("fision.nimble", fmt"""
+const
+  blockList = ["comprehension", "with"] # Packages with problems to install of some kind.
+  url = "https://raw.githubusercontent.com/nim-lang/Nim/devel/testament/important_packages.nim"
+  code = staticExec("curl " & url).strip.splitLines
+
+var packages = code
+packages = filterIt(packages, (it.startsWith("pkg1 ") or it.startsWith("pkg2 ")) and (it.contains(", false") or not it.contains(", true")))
+packages = mapIt(packages, it.toLowerAscii.split(", false" )[0].replace("pkg1 ", "").replace("pkg2 ", "").strip)
+packages = filterIt(packages, it notin blockList).sorted
+assert packages.len > 0
+
+writeFile("fision.nimble", fmt"""
 requires "nim >= { NimVersion }",
-  {packages}
+  { packages.join(",\n  ") }
 #END { CompileDate }T{ CompileTime }
 
 version     = "{ CompileDate.replace("-", ".") }"
