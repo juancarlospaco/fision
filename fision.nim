6cb0c58ec4449aed27f6664bdc9bbc86b7a7d799
Juan Carlos <JuanCarlospaco@gmail.com>
2020-06-22 03:54:26 -0300

    init

--- fision.nim
+++ fision.nim
@@ -5,14 +5,10 @@ static:
     blockList = ["comprehension", "with"] # Packages with problems to install of some kind.
     url = "https://raw.githubusercontent.com/nim-lang/Nim/devel/testament/important_packages.nim"
     code = staticExec("curl " & url).strip.splitLines
-    packages = block:
-      var result: seq[string]
-      for it in filterIt(code, (it.startsWith("pkg1 ") or it.startsWith("pkg2 ")) and (it.contains(", false") or not it.contains(", true"))):
-        result.add it.toLowerAscii.split(", false" )[0].replace("pkg1 ", "").replace("pkg2 ", "").strip
-      result = filterIt(result, it notin blockList)
-      echo result.len
-      result.sorted.join(",\n  ")
-  when code.len > 0 and  packages.len > 0: writeFile("fision.nimble", fmt"""
+    filter0 = filterIt(code, (it.startsWith("pkg1 ") or it.startsWith("pkg2 ")) and (it.contains(", false") or not it.contains(", true")))
+    filter1 = map(filter0, proc(s: string): string = s.toLowerAscii.split(", false" )[0].replace("pkg1 ", "").replace("pkg2 ", "").strip)
+    packages = filterIt(filter1, it notin blockList).sorted.join(",\n  ")
+  when code.len > 0 and packages.len > 0: writeFile("fision.nimble", fmt"""
 requires "nim >= { NimVersion }",
   {packages}
 #END { CompileDate }T{ CompileTime }
