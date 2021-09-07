376177048b53a42198b3ab95324f317ac0dda082
Juan Carlos <juancarlospaco@gmail.com>
2021-09-07 19:26:30 -0300

    revive

--- fision.nim
+++ fision.nim
@@ -1,12 +1,12 @@
-import strutils, sequtils, algorithm, strformat
+import std/[strutils, sequtils, algorithm, strformat]
 
 const
   blockList = ["\"comprehension\"", "\"with\"", "\"kdtree\"", "\"snip\"", "\"tensordsl\"", "\"bigints\""]  # Packages flaky to install(?).
   url = "https://raw.githubusercontent.com/nim-lang/Nim/devel/testament/important_packages.nim"
   code = staticExec("curl " & url).strip.splitLines
-  pkgs = filterIt(code, (it.startsWith("pkg1 ") or it.startsWith("pkg2 ")) and (it.contains(", false") or not it.contains(", true")))
-  fltr = map(pkgs, proc(s: string): string = s.toLowerAscii.split(", false" )[0].replace("pkg1 ", "").replace("pkg2 ", "").strip)
-  packages = filterIt(fltr, it notin blockList and it.len > 0).sorted.join(",\n  ")
+  pkgs = filterIt(code, it.startsWith"pkg " and not it.contains"allowFailure")
+  fltr = map(pkgs, proc(s: string): string = s.toLowerAscii.split(", ")[0].replace("pkg ", "").strip)
+  packages = filterIt(fltr, it notin blockList and it.len > 0).sorted.join",\n"
 
 static:
   echo packages
